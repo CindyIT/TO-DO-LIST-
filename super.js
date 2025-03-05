6c8f78e783a7881dda24a9308dfa36d8aeac071e
makhuga cindy <221181229@edu.vut.ac.za>
2025-03-05 10:30:48 +0200

    Added functionality for adding tasks to the list, including the ability to mark tasks as completed or remove them via right-click. Updated local storage after each action to persist changes. ✅📝💻
    added needed functions

--- super.js
+++ super.js
@@ -13,3 +13,54 @@ if (todos) {
     todos.forEach(todo => addTodo(todo));
 
 }
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault();  
+    
+    addTodo();  
+});
+
+
+function addTodo(todo = null) {
+
+    let todoText = input.value;
+
+    if (todo) {
+
+        todoText = todo.text;
+
+    }
+
+    if (todoText) {
+        
+        const todoEl = document.createElement('li');
+
+
+        if (todo && todo.completed) {
+
+            todoEl.classList.add('completed');
+   
+        }
+
+   
+        todoEl.innerText = todoText;
+
+   
+        todoEl.addEventListener('click', () => {
+   
+            todoEl.classList.toggle('completed');
+   
+            updateLS();
+   
+        });
+
+        
+        todoEl.addEventListener('contextmenu', (e) => {
+        
+            e.preventDefault(); 
+       
+            todoEl.remove();
+        
+            updateLS();
+        
+        });
