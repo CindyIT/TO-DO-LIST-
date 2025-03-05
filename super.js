c189ecf34ff973d5f37344a663bb8522d0d1521c
makhuga cindy <221181229@edu.vut.ac.za>
2025-03-05 10:28:16 +0200

    Implemented functionality to load tasks from local storage, ensuring that previously saved todos persist when the app is reopened. 📦💾
    added my js

---
+++ super.js
@@ -0,0 +1,15 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todosUl = document.getElementById('todos');
+
+
+// Load todos from local storage
+
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+
+if (todos) {
+
+    todos.forEach(todo => addTodo(todo));
+
+}
